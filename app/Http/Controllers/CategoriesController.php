4feeca56cb751830464492f193f37e249f153426
glriadomenica-debug <glriadomenica@gmail.com>
2026-05-12 21:20:36 +0700

    update categories controller

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Helpers\ApiMessage;
 use App\Models\Categories;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class CategoriesController extends Controller
